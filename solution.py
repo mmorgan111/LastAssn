6499bd8ccdcca5283dba0ebd1d844819e7a6841d
mmorgan111 <112628435+mmorgan111@users.noreply.github.com>
2022-11-19 12:40:36 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -148,12 +148,18 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     #Fill in start
                     #You should update your dataframe with the required column field responses here
-                    print (" %d   rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]))
+                    #print (" %d   rtt=%.0f ms %s" % (ttl,(timeReceived -t)*1000, addr[0]))
+                    df = df.append({'Hop Count': str(ttl), 'Try': str(icmp), 'IP': str(destAddr), 'Hostname': str(hostname), 'Response Code': str(types)},
+                            ignore_index=True)
+                    print(df)
                     #Fill in end
                 elif types == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    print (" %d   rtt=%.0f ms %s" % (ttl,(timeReceived -timeSent)*1000, addr[0]))
+                    #print (" %d   rtt=%.0f ms %s" % (ttl,(timeReceived -timeSent)*1000, addr[0]))
+                    df = df.append({'Hop Count': str(ttl), 'Try': str(icmp), 'IP': str(destAddr), 'Hostname': str(hostname), 'Response Code': str(types)},
+                            ignore_index=True)
+                    print(df)
                     #Fill in start
                     #You should update your dataframe with the required column field responses here
                     #Fill in end
