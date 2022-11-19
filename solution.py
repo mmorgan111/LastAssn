7372e5b65182884ea3e5a84921c09af89f4062a8
mmorgan111 <112628435+mmorgan111@users.noreply.github.com>
2022-11-19 12:22:39 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -112,7 +112,7 @@ def get_route(hostname):
                 #Fill in end
                 try: #try to fetch the hostname
                     #Fill in start
-                    hostname = gethostbyname(hostname)
+                    hostname = hostname
                     #Fill in end
                 except herror:   #if the host does not provide a hostname
                     #Fill in start
